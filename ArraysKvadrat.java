16af9c4930c0cdb7821e930ac094a129b460f270
serhio-w01 <serh.w01>
2015-02-26 15:45:36 +0100

    dodana toString(long[][]
    toString za tabele z elementi tipa long

--- ArraysKvadrat.java
+++ ArraysKvadrat.java
@@ -53,6 +53,31 @@ public class ArraysKvadrat
         ss+=" ]";
         return ss;
     }
+	
+	/**
+     *  Vrne besedilno interpretacijo tabele, podane z argumentom. Ce argument ni tabela ali
+     *  ni dvorazsezna 'kvadratna' tabela, mora vrniti 'null'.
+     *  @param ar tabela, ki jo skušamo predstaviti
+     *  @return String ki predstavlja to tabelo
+     *  @since 7
+     *  
+     *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
+     */  
+    public static String toString(long[][] ar){
+         
+		if (ar == null) return "null";
+		if (ar[0] == null) return "null";
+		//if (ar[0].length != ar.length) return "null"; // tale ni zadosten, tudi vse ostale
+		 
+		int arl = ar.length; 
+        String ss="[ ";
+        for(int i=0;i<ar.length;i++){
+		  if(arl != ar[i].length) return "null";	// ce ni kvadratna
+          ss += (i==0)? Arrays.toString(ar[i]):" , "+ Arrays.toString(ar[i]);
+		}
+        ss+=" ]";
+        return ss;
+    }
 }
 
 
