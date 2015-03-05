bf8799686f18993cd43335c29bbed9bf77cc8270
tiwyeagle <aljaz.erzen@gmail.com>
2015-03-05 02:12:44 +0100

    Dopolnitev in popravek intendacije

--- ArraysKvadrat.java
+++ ArraysKvadrat.java
@@ -10,74 +10,82 @@
 
 import java.util.Arrays;
 
-public class ArraysKvadrat
-{
-    
-    // zagotovimo, da se ne da narediti objekta iz tega; privzeti konstruktor je privaten
-      private ArraysKvadrat(){
-      }
-    
-      
-    /**
-     *  Vrne besedilno interpretacijo tabele, podane z argumentom. Če argument ni tabela ali
-     *  ni dvorazsežna 'kvadratna' tabela, mora vrniti 'null'.
-     *  @param ar tabela, ki jo skušamo predstaviti
-     *  @return String ki predstavlja to tabelo
-     *  @since 7
-     *  
-     *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
-     */  
-    public static String toString(int[][] ar){
-         
-        String ss="[ ";
-        for(int i=0;i<ar.length;i++)
-          ss += (i==0)? Arrays.toString(ar[i]):" , "+ Arrays.toString(ar[i]);
-        ss+=" ]";
-        return ss;
-    }
-    
-    /**
-     *  Vrne besedilno interpretacijo tabele, podane z argumentom. Če argument ni tabela ali
-     *  ni dvorazsežna 'kvadratna' tabela, mora vrniti 'null'.
-     *  @param ar tabela, ki jo skušamo predstaviti
-     *  @return String ki predstavlja to tabelo
-     *  @since 7
-     *  
-     *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
-     */  
-    public static String toString(char[][] ar){
-         
-        String ss="[ ";
-        for(int i=0;i<ar.length;i++)
-          ss += (i==0)? Arrays.toString(ar[i]):" , "+ Arrays.toString(ar[i]);
-        ss+=" ]";
-        return ss;
-    }
+public class ArraysKvadrat {
 	
+	// zagotovimo, da se ne da narediti objekta iz tega; privzeti konstruktor je privaten
+	public ArraysKvadrat() {
+	}
+	  
 	/**
-     *  Vrne besedilno interpretacijo tabele, podane z argumentom. Ce argument ni tabela ali
-     *  ni dvorazsezna 'kvadratna' tabela, mora vrniti 'null'.
-     *  @param ar tabela, ki jo skušamo predstaviti
-     *  @return String ki predstavlja to tabelo
-     *  @since 7
-     *  
-     *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
-     */  
-    public static String toString(long[][] ar){
-         
+	 *  Vrne besedilno interpretacijo tabele, podane z argumentom. Če argument ni tabela ali
+	 *  ni dvorazsežna 'kvadratna' tabela, mora vrniti 'null'.
+	 *  @param ar tabela, ki jo skušamo predstaviti
+	 *  @return String ki predstavlja to tabelo
+	 *  @since 7
+	 *  
+	 *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
+	 */  
+	public static String toString(int[][] ar){     
 		if (ar == null) return "null";
-		if (ar[0] == null) return "null";
-		//if (ar[0].length != ar.length) return "null"; // tale ni zadosten, tudi vse ostale
 		 
 		int arl = ar.length; 
-        String ss="[ ";
-        for(int i=0;i<ar.length;i++){
-		  if(arl != ar[i].length) return "null";	// ce ni kvadratna
-          ss += (i==0)? Arrays.toString(ar[i]):" , "+ Arrays.toString(ar[i]);
+		String ss="[ ";
+		for(int i=0; i<ar.length; i++) {
+			if(ar[i] == null) return "null";		// ena izmed vrstic ne obstaja
+			if(arl != ar[i].length) return "nonSquare";	// ce ni kvadratna
+			ss += (i==0?"":" , ")+ Arrays.toString(ar[i]);
 		}
-        ss+=" ]";
-        return ss;
-    }
+		ss+=" ]";
+		return ss;
+	}
+
+	/**
+	 *  Vrne besedilno interpretacijo tabele, podane z argumentom. Če argument ni tabela ali
+	 *  ni dvorazsežna 'kvadratna' tabela, mora vrniti 'null'.
+	 *  @param ar tabela, ki jo skušamo predstaviti
+	 *  @return String ki predstavlja to tabelo
+	 *  @since 7
+	 *  
+	 *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
+	 */  
+	public static String toString(char[][] ar){
+		if (ar == null) return "null";
+		 
+		int arl = ar.length; 
+		String ss="[ ";
+		for(int i=0;i<ar.length;i++) {
+			if(ar[i] == null) return "null";		// ena izmed vrstic ne obstaja
+			if(arl != ar[i].length) return "nonSquare";	// ce ni kvadratna
+			ss += (i==0? "":" , ") + Arrays.toString(ar[i]);
+		}
+		ss+=" ]";
+		return ss;
+	}
+	
+	/**
+	 *  Vrne besedilno interpretacijo tabele, podane z argumentom. Ce argument ni tabela ali
+	 *  ni dvorazsezna 'kvadratna' tabela, mora vrniti 'null'.
+	 *  @param ar tabela, ki jo skušamo predstaviti
+	 *  @return String ki predstavlja to tabelo
+	 *  @since 7
+	 *  
+	 *  ref: http://developer.classpath.org/doc/java/util/Arrays-source.html in http://www.docjar.com/html/api/java/util/Arrays.java.html
+	 */  
+	public static String toString(long[][] ar){
+		 
+		if (ar == null) return "null";
+		//if (ar[0].length != ar.length) return "null"; // tale ni zadosten, tudi vse ostale
+
+		int arl = ar.length; 
+		String ss="[ ";
+		for(int i=0;i<ar.length;i++){
+			if(ar[i] == null) return "null";		// ena izmed vrstic ne obstaja
+			if(arl != ar[i].length) return "nonSquare";	// ce ni kvadratna
+			ss += (i==0? "":" , ")+ Arrays.toString(ar[i]);
+		}
+		ss+=" ]";
+		return ss;
+	}
 }
 
 
